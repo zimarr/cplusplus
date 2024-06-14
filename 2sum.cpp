0da23ad1a9390faddd9420f5377e83474a224798
zimarr <zimaraksoy@gmail.com>
2024-06-14 15:03:34 -0500

    First excersise complete

---
+++ 2sum.cpp
@@ -0,0 +1,17 @@
+/*
+Evaluates the sum of 2 integers inputted by the user.
+*/
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a, b;
+
+    cout << "\nInput first number: ";
+    cin >> a;
+
+    cout << "Input second number: ";
+    cin >> b;
+
+    cout << "\nThe sum of the two numbers is " << a + b << endl;
+}
